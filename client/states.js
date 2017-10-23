41bc6cbb70eafbc60b7d94706df58e27f9670e81
Delgado Chen <delgado.chen@flex.com>
2017-10-23 01:35:58 -0700

    [Studio] Add screen "TableView Advanced-Confident"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.tableviewadvancedconfident', {
+        views: {
+            app: {
+                controller: 'app_tableviewadvancedconfident',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.tableviewadvancedconfident');
+                }
+            }
+        }
     });
 });
