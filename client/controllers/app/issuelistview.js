599a20f6615d4a575ae07ae0ece00da7c8b0807d
Delgado Chen <delgado.chen@flex.com>
2017-10-25 01:18:44 -0700

    [Studio] Add screen "issuelistview"

---
+++ client/controllers/app/issuelistview.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_issuelistview', app_issuelistview);
+function app_issuelistview($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.issuelistview', {
+        views: {
+            app: {
+                controller: 'app_issuelistview',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.issuelistview');
+                }
+            }
+        }
     });
 });
